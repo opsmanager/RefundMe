c202c506ea5f1fd655678431767ed9c3106d0a1f
Brennan Neoh and Sam Kah Chiin <pair+brennan+sam@opsmanager.com>
2016-09-05 17:36:48 +0800

    WIP - added camera.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,7 @@
+import {Camera} from 'ionic-native';
+
 angular.module('app.controllers', [])
-        
+
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -7,7 +9,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,7 +17,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('myReceiptsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,15 +25,25 @@ function ($scope, $stateParams) {
 
 
 }])
-   
-.controller('receiptDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('receiptDetailsCtrl', ['$scope', '$stateParams' // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+  $scope.takePicture = function (options) {
+    var options = {
+      quality: 75,
+      targetWidth: 200,
+      targetHeight: 200,
+      sourceType: 1
+    };
 
-
+    Camera.getPicture(options).then(function(imageData) {
+      $scope.picture = imageData;
+    }, (err) => { console.log(err); });
+  };
 }])
-   
+
 .controller('cameraViewCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -39,7 +51,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('addReceiptDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -47,4 +59,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
